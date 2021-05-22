773685cc2292f7c7442a361dc202aaef06cffc17
manhhungpc <phamhung2002@gmail.com>
2021-05-22 16:04:44 +0700

    create Subcribe form and footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,7 +2,53 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footers">
+      <div className="memoravel-infor">
+        <div className="title-footer infor-footer">
+          <div style={{ fontSize: "22px" }}>MEMORAVEL</div>
+          <p style={{ width: "250px" }}>
+            Get out there &amp; discover your next slope, mountain &amp; destination!
+          </p>
+        </div>
+        <small className="copyright">Copyright 2021 MEMORAVEL, Inc. Terms &amp; Privacy</small>
+      </div>
+      <div className="memoravel-link">
+        <div className="title-footer-link">More on the blog</div>
+        <ul>
+          <li className="link-to-page">
+            <a href="#">About MEMORAVEL</a>
+          </li>
+          <li className="link-to-page">
+            <a href="#">Contributors &amp; Writers</a>
+          </li>
+          <li className="link-to-page">
+            <a href="#">Write for us</a>
+          </li>
+          <li className="link-to-page">
+            <a href="#">Contact us</a>
+          </li>
+          <li className="link-to-page">
+            <a href="#">Privacy Policy</a>
+          </li>
+        </ul>
+      </div>
+      <div className="memoravel-link">
+        <div className="title-footer-link">More on MEMORAVEL</div>
+        <ul>
+          <li className="link-to-page">
+            <a href="#">The Team</a>
+          </li>
+          <li className="link-to-page">
+            <a href="#">Jobs</a>
+          </li>
+          <li className="link-to-page">
+            <a href="#">Press</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- client/src/components/SubcribeForm/SubcribeForm.js
+++ client/src/components/SubcribeForm/SubcribeForm.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./SubcribeForm.css";
 
 function SubcribeForm() {
-  return <div className="sub-form-wrapper">{/* Todo */}</div>;
+  return (
+    <div className="sub-form-wrapper">
+      <div className="header-form">MEMORAVEL.VN</div>
+      <div className="subcribe-text">Subcribe Form</div>
+      <div className="subcribe-input">
+        <input className="email-form" placeholder="Email address" type="text" />
+        <button className="button-form">Submit</button>
+      </div>
+    </div>
+  );
 }
 
 export default SubcribeForm;
